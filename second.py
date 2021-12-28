17e2c81c197d6caee39bf96cf61d3c3440a99d01
thaiscaguiar <thaiscaguiar@hotmail.com>
2021-12-28 10:32:02 -0300

    INCLUSAO DE COMENTARIOS

--- second.py
+++ second.py
@@ -1,96 +1,101 @@
-from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions
 from pyspark.sql.types import FloatType
-from pyspark.sql.functions import col, count
+from pyspark.sql.functions import col
 import mysql.connector
-from datetime import datetime
 import pandas as pd
-import numpy as np
-
 
 caminho_csv = "gs://arquivo_csv"
 caminho_parquet = "gs://arquivo_parquet"
 
+if __name__ == "__main__":
+    try:
+        # CONEXAO COM O MYSQL
+        con = mysql.connector.connect(user='root', password='projetoNatal', host='10.6.208.3', database='consumo_combustivel')
+        cursor = con.cursor()
 
-con = mysql.connector.connect(user='root', password='projetoNatal', host='10.6.208.3', database='consumo_combustivel')
-cursor = con.cursor()
-
-query_2015 = "SELECT * FROM consumo_2015;"
-query = cursor.execute(query_2015)
-query = cursor.fetchall()
-df_2015 = pd.DataFrame(query)
-
-
-query_2016 = "select * from consumo_2016;"
-query = cursor.execute(query_2016)
-query = cursor.fetchall()
-df_2016 = pd.DataFrame(query)
+        # SELECT NAS PLANILHAS E SALVA COMO DATAFRAME COM PANDAS
+        query_2015 = "SELECT * FROM consumo_2015;"
+        query = cursor.execute(query_2015)
+        query = cursor.fetchall()
+        df_2015 = pd.DataFrame(query)
 
 
-query_2017 = "select * from consumo_2017;"
-query = cursor.execute(query_2017)
-query = cursor.fetchall()
-df_2017 = pd.DataFrame(query)
+        query_2016 = "select * from consumo_2016;"
+        query = cursor.execute(query_2016)
+        query = cursor.fetchall()
+        df_2016 = pd.DataFrame(query)
 
 
-query_2018 = "select * from consumo_2018;"
-query = cursor.execute(query_2018)
-query = cursor.fetchall()
-df_2018 = pd.DataFrame(query)
+        query_2017 = "select * from consumo_2017;"
+        query = cursor.execute(query_2017)
+        query = cursor.fetchall()
+        df_2017 = pd.DataFrame(query)
 
 
-query_2019 = "select * from consumo_2019;"
-query = cursor.execute(query_2019)
-query = cursor.fetchall()
-df_2019 = pd.DataFrame(query)
+        query_2018 = "select * from consumo_2018;"
+        query = cursor.execute(query_2018)
+        query = cursor.fetchall()
+        df_2018 = pd.DataFrame(query)
 
 
-query_2020 = "select * from consumo_2020;"
-query = cursor.execute(query_2020)
-query = cursor.fetchall()
-df_2020 = pd.DataFrame(query)
+        query_2019 = "select * from consumo_2019;"
+        query = cursor.execute(query_2019)
+        query = cursor.fetchall()
+        df_2019 = pd.DataFrame(query)
 
 
-cursor.close()
-con.commit()
-con.close()
-
-df_total = pd.concat([df_2015, df_2016, df_2017, df_2018, df_2019, df_2020])
-
-df_total.columns = ["id_total","regiao_sigla","estado_sigla","municipio","revenda","CNPJ_revenda","nome_da_rua","numero_rua","complemento","bairro","cep","produto","data_da_coleta","valor_de_venda","valor_de_compra","unidade_de_medida","bandeira"]
-
-df_total.drop(['id_total'], axis=1, inplace=True)
-
-#Inicia sessão no spark
-spark = SparkSession.builder.appName("OTR").config("spark.sql.caseSensitive", "True").getOrCreate()
-
-df_total.to_csv(f"{caminho_csv}/concat.csv", header=True)
+        query_2020 = "select * from consumo_2020;"
+        query = cursor.execute(query_2020)
+        query = cursor.fetchall()
+        df_2020 = pd.DataFrame(query)
 
 
-df = spark.read.format("csv")\
-    .option("header", "true")\
-    .option("delimiter", ",")\
-    .option("inferSchema", "true")\
-    .load(f"{caminho_csv}/concat.csv")
+        cursor.close()
+        con.commit()
+        con.close()
 
-converter_valor = lambda variavel: float(variavel.replace(",","."))
-udf_converter_valor = functions.udf(converter_valor,FloatType())
+        # CONCATENA AS PLANILHAS
+        df_total = pd.concat([df_2015, df_2016, df_2017, df_2018, df_2019, df_2020])
 
+        df_total.columns = ["id_total","regiao_sigla","estado_sigla","municipio","revenda","CNPJ_revenda","nome_da_rua","numero_rua","complemento","bairro","cep","produto","data_da_coleta","valor_de_venda","valor_de_compra","unidade_de_medida","bandeira"]
 
-resultado = df.withColumn("valor_de_venda", udf_converter_valor(df["valor_de_venda"]))
-resultado = resultado.withColumn("valor_de_compra", udf_converter_valor(resultado["valor_de_compra"]))
+        # DROPA COLUNA ID_TOTAL
+        df_total.drop(['id_total'], axis=1, inplace=True)
 
-df_total = resultado.select(col("regiao_sigla")\
-,col("estado_sigla")\
-,col("municipio")\
-,col("revenda")\
-,col("CNPJ_revenda")\
-,col("produto")\
-,col("data_da_coleta")\
-,col("valor_de_venda")\
-,col("valor_de_compra")\
-,col("bandeira"))
-df_total.write.parquet(f"{caminho_parquet}/total_parquet")
+        # CONEXAO COM SPARK
+        spark = SparkSession.builder.appName("OTR").config("spark.sql.caseSensitive", "True").getOrCreate()
 
-print("Fim da execução")
+        # LE ARQUIVO CSV
+        df_total.to_csv(f"{caminho_csv}/concat.csv", header=True)
+
+        df = spark.read.format("csv")\
+            .option("header", "true")\
+            .option("delimiter", ",")\
+            .option("inferSchema", "true")\
+            .load(f"{caminho_csv}/concat.csv")
+
+        # TRATAMENTO DO CSV SUBSTITUINDO PONTO POR VIRGULA NAS COLUNAS COM VALORES
+        converter_valor = lambda variavel: float(variavel.replace(",","."))
+        udf_converter_valor = functions.udf(converter_valor,FloatType())
+
+        resultado = df.withColumn("valor_de_venda", udf_converter_valor(df["valor_de_venda"]))
+        resultado = resultado.withColumn("valor_de_compra", udf_converter_valor(resultado["valor_de_compra"]))
+
+        # SALVA ARQUIVO PARQUET
+        df_total = resultado.select(col("regiao_sigla")\
+        ,col("estado_sigla")\
+        ,col("municipio")\
+        ,col("revenda")\
+        ,col("CNPJ_revenda")\
+        ,col("produto")\
+        ,col("data_da_coleta")\
+        ,col("valor_de_venda")\
+        ,col("valor_de_compra")\
+        ,col("bandeira"))
+        df_total.write.parquet(f"{caminho_parquet}/total_parquet")
+
+        print("Fim da execução")
+        
+    except Exception as e:
+        print(str(e))
--- third.py
+++ third.py
@@ -6,25 +6,34 @@ import pandas as pd
 
 caminho_parquet = "gs://arquivo_parquet/total_parquet"
 
-clstr = Cluster(['34.151.227.87'], port=9042)
-session = clstr.connect('analise_combustivel')
+if __name__ == "__main__":
+    try:
+        # CONEXAO COM CASSANDRA
+        clstr = Cluster(['34.151.227.87'], port=9042)
+        session = clstr.connect('analise_combustivel')
 
-spark = SparkSession.builder.appName("OTR").config("spark.sql.caseSensitive", "True").getOrCreate()
+        # CONEXAO COM SPARK
+        spark = SparkSession.builder.appName("OTR").config("spark.sql.caseSensitive", "True").getOrCreate()
 
-df_total = spark.read.parquet(f"{caminho_parquet}")
+        # LE ARQUIVO PARQUET
+        df_total = spark.read.parquet(f"{caminho_parquet}")
 
-lista_dados = df_total.filter("CNPJ_revenda != '0'").collect()
-df_dados = pd.DataFrame(lista_dados)
-df_dados.columns = ["regiao_sigla","estado_sigla","municipio","revenda","CNPJ_revenda","produto","data_da_coleta","valor_de_venda","valor_de_compra","bandeira"]
+        lista_dados = df_total.filter("CNPJ_revenda != '0'").collect()
+        df_dados = pd.DataFrame(lista_dados)
+        df_dados.columns = ["regiao_sigla","estado_sigla","municipio","revenda","CNPJ_revenda","produto","data_da_coleta","valor_de_venda","valor_de_compra","bandeira"]
 
-for index, row in df_dados.iterrows():
-    
-    if pd.isnull(row.valor_de_compra):
-        valores = "(uuid(),'{}','{}','{}','{}','{}','{}','{}',{},'{}')".format(row.regiao_sigla,row.estado_sigla,row.municipio, row.revenda, row.CNPJ_revenda, row.produto, row.data_da_coleta, row.valor_de_venda, row.bandeira)
-        sql = "INSERT INTO CONSUMO (id,regiao_sigla,estado_sigla,municipio, razao_social, cnpj, produto, data_da_coleta, valor_de_venda, bandeira) values "+ valores +";"
-    else:
-        valores = "(uuid(),'{}','{}','{}','{}','{}','{}','{}',{}, {},'{}')".format(row.regiao_sigla,row.estado_sigla,row.municipio, row.revenda, row.CNPJ_revenda, row.produto, row.data_da_coleta, row.valor_de_venda, row.valor_de_compra, row.bandeira)
-        sql = "INSERT INTO CONSUMO (id,regiao_sigla,estado_sigla,municipio, razao_social, cnpj, produto, data_da_coleta, valor_de_venda, valor_de_compra, bandeira) values "+ valores +";"
-    session.execute(sql)
-    
-print("Fim da execução")
+        # INSERCAO DOS DADOS NO CASSANDRA
+        for index, row in df_dados.iterrows():
+            
+            if pd.isnull(row.valor_de_compra):
+                valores = "(uuid(),'{}','{}','{}','{}','{}','{}','{}',{},'{}')".format(row.regiao_sigla,row.estado_sigla,row.municipio, row.revenda, row.CNPJ_revenda, row.produto, row.data_da_coleta, row.valor_de_venda, row.bandeira)
+                sql = "INSERT INTO CONSUMO (id,regiao_sigla,estado_sigla,municipio, razao_social, cnpj, produto, data_da_coleta, valor_de_venda, bandeira) values "+ valores +";"
+            else:
+                valores = "(uuid(),'{}','{}','{}','{}','{}','{}','{}',{}, {},'{}')".format(row.regiao_sigla,row.estado_sigla,row.municipio, row.revenda, row.CNPJ_revenda, row.produto, row.data_da_coleta, row.valor_de_venda, row.valor_de_compra, row.bandeira)
+                sql = "INSERT INTO CONSUMO (id,regiao_sigla,estado_sigla,municipio, razao_social, cnpj, produto, data_da_coleta, valor_de_venda, valor_de_compra, bandeira) values "+ valores +";"
+            session.execute(sql)
+            
+        print("Fim da execução")
+        
+    except Exception as e:
+        print(str(e))
